8be2bc935765873504fa6557da6ecb99db071357
linzm <linzm@ud361.com>
2018-11-07 16:35:26 +0800

    使用lombok修改user类

--- miaosha/src/main/java/com/synctech/domian/User.java
+++ miaosha/src/main/java/com/synctech/domian/User.java
@@ -1,25 +1,14 @@
 package com.synctech.domian;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
 public class User {
 
 	private int id;
 	private String name;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + "]";
-	}
-	
-	
 }
