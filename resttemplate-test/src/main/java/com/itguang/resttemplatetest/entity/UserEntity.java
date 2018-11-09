88fb4c0ad7b51d84003721da764032f6114c4f99
linzm <linzm@ud361.com>
2018-11-09 11:32:20 +0800

    使用lombok修改UserEntity类

--- resttemplate-test/src/main/java/com/itguang/resttemplatetest/entity/UserEntity.java
+++ resttemplate-test/src/main/java/com/itguang/resttemplatetest/entity/UserEntity.java
@@ -2,16 +2,30 @@ package com.itguang.resttemplatetest.entity;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
  * 用户实体类
  *
  * @author itguang
  * @create 2017-12-16 9:44
  **/
-
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity implements Serializable{
 
-    private String id;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String id;
 
     private String username;
 
@@ -21,65 +35,5 @@ public class UserEntity implements Serializable{
 
     private String email;
 
-    public UserEntity() {
-    }
 
-    public UserEntity(String id, String username, String password, Integer age, String email) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.age = age;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
